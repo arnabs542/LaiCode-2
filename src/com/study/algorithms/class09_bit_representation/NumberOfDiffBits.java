976dd1ea4b2e0e1549db242970965db726e5e070
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2021-01-02 14:47:37 +0800

    Create NumberOfDiffBits.java

---
+++ src/com/study/algorithms/class09_bit_representation/NumberOfDiffBits.java
@@ -0,0 +1,18 @@
+package com.study.algorithms.class09_bit_representation;
+
+// Determine the number of bits that are different for two given integers.
+// Examples:   5(“0101”) and 8(“1000”) has 3 different bits
+
+public class NumberOfDiffBits {
+  // 我感觉可以用异或 + count,  也可以一个一个比较
+  public int diffBits(int a, int b) {
+    int x = a ^ b;
+    int count = 0;
+//    while (x > 0) { 错误，可以小于0
+    while (x != 0) {
+      count += x & 1;
+      x = x >>> 1;
+    }
+    return count;
+  }
+}
