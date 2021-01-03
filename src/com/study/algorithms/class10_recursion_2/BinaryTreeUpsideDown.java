0d98b2ba356c1912117a1cd0be9c642d99f86a53
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2021-01-03 19:14:59 +0800

    solve BinaryTreeUpsideDown in a iterative way.

--- src/com/study/algorithms/class10_recursion_2/BinaryTreeUpsideDown.java
+++ src/com/study/algorithms/class10_recursion_2/BinaryTreeUpsideDown.java
@@ -2,36 +2,37 @@ package com.study.algorithms.class10_recursion_2;
 
 import com.study.util.ListNode;
 import com.study.util.TreeNode;
+import com.sun.source.tree.Tree;
 
 public class BinaryTreeUpsideDown {
   // Given a binary tree where all the right nodes are leaf nodes
   // flip it upside down and turn it into a tree with left leaf nodes as the root.
   /*
   Examples
-          1
-         / \
-        2   5
-       / \
-      3   4
+          1 - 5
+         /
+        2 - 4
+       /
+      3
   is reversed to
-            3
-           / \
-         2   4
-        / \
- left  1   5
+                                                                3
+                                                               / \
+                                                             2   4
+                                                            / \
+                                                     left  1   5
   which can be viewed as:
-       5   1   left
-        \ /
-         4   2
-          \ /
-           3
+       1
+        \
+         2 - 5
+          \
+  left     3 - 4
 
   subproblem is:
     2
     /\
    3  4
    reverse to
-   4  2
+   2  4
     \/
     3
 
@@ -58,5 +59,51 @@ public class BinaryTreeUpsideDown {
     lChild.right = rChild;
     // return the root of sub-problem as my root
     return newRoot;
+  }
+
+
+  // 在recursion方案中，首先会一猛子扎到base case。随后不断地处理掉更多的tree node
+  // root可以安心地变成指向null，因为call stack中其他frame存储了各自的root信息。
+  // iteration中不可这么安心：
+  // 1. subproblem用next来记忆着
+  // 2. 本次root要连接的是上一次loop的root和它的right （充当上文lChild的角色）
+  //    root
+  //     1 - 5
+  //    /
+  //    2 - 4
+  //   /
+  //  3
+  //
+
+  // -->
+
+  //     1
+  //    /
+  //    2 - 5
+  //   /
+  //  3 - 4
+  // root
+  //
+  public TreeNode reverse_iterative(TreeNode root) {
+    // iterative way:
+    TreeNode prev = null;
+    TreeNode prevRight = null;
+    TreeNode cur = root;
+    // current is also the prevLeft
+    while (cur != null) {
+      // 1. get the new value
+      TreeNode next = cur.left; // left will be the next node to be process
+      TreeNode right = cur.right;
+
+      // 3. leave the logic empty initially. add it after 2
+      cur.left = prev;
+      cur.right = prevRight;
+
+      // 2. update the loop var using new value, and then jump to 3.
+      prev = cur;
+      cur = next;
+      prevRight = right;
+    }
+    return prev;
   }
 }
