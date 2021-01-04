387ec9b0128c2294985c02ba66b7195600fc99c4
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2021-01-04 13:42:50 +0800

    Update LCA.java

--- src/com/study/algorithms/class10_recursion_2/tree/LCA.java
+++ src/com/study/algorithms/class10_recursion_2/tree/LCA.java
@@ -1,4 +1,57 @@
 package com.study.algorithms.class10_recursion_2.tree;
 
+import com.study.util.TreeNode;
+
 public class LCA {
+  // Given two nodes in a binary tree, find their lowest common ancestor.
+  // TreeNode doesn't have parent field.
+
+  // case 1: 两个点有隶属关系 返回更高的那个点
+  //  base case:
+  //    1. null return null
+  //    2. a or be, return a or b. --> 会覆盖掉下层传上来的那个，即当前的是LCA。
+  //  recursion rule:
+  //    1. if both child is null, return null
+  //    2. if one of them is not null, return that one
+
+  // case 2: 两个点没有隶属关系 共同祖先（left、right subtree分别可以找到one two）
+  //  base case:
+  //    1. null return null
+  //    2. found one return that one
+  //  recursion rule:
+  //    1. both null, return null
+  //    2. one is not null, return that one
+  //  * 3. both are not null(found both), I am LCA.
+
+  // case2可以囊括case1，case1中不会出现 *3的情况。
+
+  // signature:
+  // input: root, one, two
+  // output: a possible LCA or not.
+
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode one, TreeNode two) {
+    // base case:
+    if (root == null) {
+      return null;
+    }
+    if (root == one || root == two) {
+      return root;
+    }
+    // recursion rule:
+    TreeNode leftCandidate = lowestCommonAncestor(root.left, one, two);
+    TreeNode rightCandidate = lowestCommonAncestor(root.right, one, two);
+    // case 1
+    if (leftCandidate == null && rightCandidate == null) {
+      return null;
+    }
+    // case 2
+    if (leftCandidate == null || rightCandidate == null) {
+      return leftCandidate == null ? rightCandidate : leftCandidate;
+    }
+    // case 3
+    if (leftCandidate != null && rightCandidate != null) {
+      return root;
+    }
+    return null;
+  }
 }
