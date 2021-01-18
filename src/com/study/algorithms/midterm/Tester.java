49a569eb322ecd9cda99dbaeecff4ed52b77b994
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2021-01-18 16:25:34 +0800

    Create Tester.java

---
+++ src/com/study/algorithms/midterm/Tester.java
@@ -0,0 +1,13 @@
+package com.study.algorithms.midterm;
+
+public class Tester {
+
+  public static void main(String[] args) {
+    // Assumption:
+
+    // Method:
+
+    // Time:
+    // Space:
+  }
+}
