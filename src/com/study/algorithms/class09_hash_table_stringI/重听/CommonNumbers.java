1ec7dc7537861fd516eeb0c675de13937768a104
麻尧崴 <ma_yaowei@outlook.com>
2021-06-09 10:40:12 +0800

    feat

---
+++ src/com/study/algorithms/class09_hash_table_stringI/重听/CommonNumbers.java
@@ -0,0 +1,13 @@
+package com.study.algorithms.class09_hash_table_stringI.重听;
+
+import java.util.*;
+
+public class CommonNumbers {
+//    // a和b差不多长用这个。O(min(m,n))
+//    // a.length >> b.length. 那还不如在a里面做binary search。开销O(bloga)
+//    public List<Integer> common(int[] a, int[] b) {
+//        int i = 0; // ptr for a
+//        int j = 0; // ptr for b
+//
+//    }
+}
